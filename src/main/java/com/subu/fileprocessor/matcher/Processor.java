6f85779c015189c4f9e2cbe7dd33a3b634bda1ef
thalaivar-subu <vesubramanian1996@gmail.com>
2024-06-24 13:26:38 +0530

    Offset Fix

--- src/main/java/com/subu/fileprocessor/matcher/Processor.java
+++ src/main/java/com/subu/fileprocessor/matcher/Processor.java
@@ -3,7 +3,6 @@ package com.subu.fileprocessor.matcher;
 import com.subu.fileprocessor.configuration.SharedVariableManager;
 import com.subu.fileprocessor.dao.Batch;
 import com.subu.fileprocessor.dao.Offset;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,36 +20,39 @@ public class Processor implements Callable<HashMap<String, ArrayList<Offset>>> {
     private final SharedVariableManager sharedVariableManager;
     private final AtomicInteger charNumber = new AtomicInteger(0);
     private final AtomicInteger lineNumber = new AtomicInteger(0);
-    ;
-
-    @PostConstruct
-    private void init() {
-        lineNumber.addAndGet((batch.getNumber() - 1) * 1000);
-    }
 
     @Override
-    public synchronized HashMap<String, ArrayList<Offset>> call() {
-        log.debug("Matcher Batch Number: {}, threadNumber: {}", batch.getNumber(), Thread.currentThread().getName());
-        HashMap<String, ArrayList<Offset>> offsetMap = new HashMap<>();
-        batch.getList().forEach(line -> {
-            if (!line.trim().isEmpty()) {
-                sharedVariableManager.getInputTextMap().forEach(word -> {
-                    int wordIndex = line.toLowerCase().indexOf(word);
-                    if (wordIndex != -1) {
-                        int lineOffset = sharedVariableManager.getOverallLineOffset().get() + lineNumber.incrementAndGet();
-                        int charOffset = sharedVariableManager.getOverallCharOffset().get() + wordIndex;
-                        Offset newOffset = new Offset(lineOffset, charOffset);
-                        ArrayList<Offset> offset = offsetMap.containsKey(word) ? offsetMap.get(word) : new ArrayList<>();
-                        offset.add(newOffset);
-                        offsetMap.put(word, offset);
-                    }
-                });
-            }
-            lineNumber.getAndIncrement();
-            charNumber.addAndGet(line.length());
-        });
-        sharedVariableManager.getOverallLineOffset().addAndGet(lineNumber.get());
-        sharedVariableManager.getOverallCharOffset().addAndGet(charNumber.get());
-        return offsetMap;
+    public HashMap<String, ArrayList<Offset>> call() {
+        synchronized (sharedVariableManager) {
+            log.debug("Matcher Batch Number: {}, threadNumber: {}", batch.getNumber(), Thread.currentThread().getName());
+            HashMap<String, ArrayList<Offset>> offsetMap = new HashMap<>();
+            batch.getList().forEach(tempLine -> {
+                lineNumber.incrementAndGet();
+                String line = tempLine.trim();
+                if (!line.isEmpty()) {
+                    sharedVariableManager.getInputTextMap().forEach(word -> {
+                        int wordIndex = line.toLowerCase().indexOf(word);
+                        if (wordIndex != -1 && isExactMatch(line, wordIndex, word)) {
+                            int lineOffset = sharedVariableManager.getOverallLineOffset().get() + lineNumber.get();
+                            int charOffset = sharedVariableManager.getOverallCharOffset().get() + wordIndex;
+                            Offset newOffset = new Offset(lineOffset, charOffset);
+                            ArrayList<Offset> offset = offsetMap.containsKey(word) ? offsetMap.get(word) : new ArrayList<>();
+                            offset.add(newOffset);
+                            offsetMap.put(word, offset);
+                        }
+                    });
+                }
+                charNumber.addAndGet(line.length());
+            });
+            sharedVariableManager.getOverallLineOffset().addAndGet(lineNumber.get());
+            sharedVariableManager.getOverallCharOffset().addAndGet(charNumber.get());
+            return offsetMap;
+        }
+    }
+
+    private boolean isExactMatch(String line, int index, String word) {
+        boolean beforeBoundary = (index == 0 || !Character.isLetterOrDigit(line.charAt(index - 1)));
+        boolean afterBoundary = (index + word.length() == line.length() || !Character.isLetterOrDigit(line.charAt(index + word.length())));
+        return beforeBoundary && afterBoundary;
     }
 }
